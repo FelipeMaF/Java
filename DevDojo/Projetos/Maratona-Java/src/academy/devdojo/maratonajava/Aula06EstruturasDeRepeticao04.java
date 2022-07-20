d1479da50e72d5b74a000b3c7cdbf215592b904d
FelipeMaF <femf20795@gmail.com>
2022-07-20 20:02:18 -0300

    Exercicio

---
+++ DevDojo/Projetos/Maratona-Java/src/academy/devdojo/maratonajava/Aula06EstruturasDeRepeticao04.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava;
+
+public class Aula06EstruturasDeRepeticao04 {
+    public static void main(String[] args) {
+        //Dado o valor de um carro, descubra em quantas x ele pode ser parcelado
+        //Condição valorParcela >= 1000
+
+        double valorCarro = 50000;
+        double valorParcela = 1000;
+
+
+
+    }
+}
